21c301d1ada6ae83b2fb216df64adbca28c2bb0c
victerr <ivorra.alberola@gmail.com>
2020-02-07 09:50:58 +0100

    Últimos arreglos en rutas

--- routes/auth.js
+++ routes/auth.js
@@ -19,7 +19,7 @@ router.post('/login', (req, res) => {
         
     if (existeUsuario.length > 0) {
         req.session.usuario = existeUsuario[0].login;
-        res.redirect('/recetas');
+        res.redirect('/admin/recetas');
     } else {
         res.render('auth_login',
             { error: "Usuario incorrecto" });
